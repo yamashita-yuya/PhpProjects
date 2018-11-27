7bf961d685e7ec19a228b003d0d726276dceba5a
yamashita-yuya <yama.98904@gmail.com>
2018-11-27 22:03:47 +0900

    fix phone-number auto-molding

--- JsElementary/auto-molding/main.js
+++ JsElementary/auto-molding/main.js
@@ -1,7 +1,7 @@
 $(function(){
   $(".format-number").change(function(){
     var format_before = $(this).val();
-    format_before = format_before.replace('-','');
+    format_before = format_before.replace(/-/g,'');
     var format_after = format_before.replace(/[Ａ-Ｚａ-ｚ０-９]/g,function(s){return String.fromCharCode(s.charCodeAt(0)-0xFEE0)});
     if(format_after.length == 11){
       $(this).val(format_after.substr(0,3)+'-'+format_after.substr(3,4)+'-'+format_after.substr(7,4));
